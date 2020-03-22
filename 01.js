642e4a8c3feeeb23588ee57ea4ce6aba8dadf767
Dzaki Buchori <dzakibuchori@gmail.com>
2020-03-22 23:48:16 +0700

    as challenge expectation output

--- 01.js
+++ 01.js
@@ -1,12 +1,12 @@
 /* GRADE */
 let i = prompt('Input','');
 
-let Grade = function(i){
+let grade = function(i){
   return (i >= 90 ? 'A' : 
     (i >= 80 && i <= 89)  ? 'B' : 
     (i >= 70 && i <= 79)  ? 'C' : 
     (i >= 60 && i <= 69)  ? 'D' : 
     (i <= 69)  ? 'E' : 'input is not valid' );
 }
-
-console.log('Output : '+ Grade(i));
+console.log('Input : grade(' + i + ')');
+console.log('Output : ' + Grade(i));
