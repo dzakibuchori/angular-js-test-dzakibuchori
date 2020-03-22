742b8f63760b6a889da727ecd12c430d6d1fcd67
Dzaki Buchori <dzakibuchori@gmail.com>
2020-03-22 23:50:25 +0700

    02 Reverse

---
+++ 02.js
@@ -0,0 +1,8 @@
+/* REVERSE */
+let i = prompt('Input','');
+
+let reverse = function(i){
+  return i.split("").reverse().join("");
+}
+console.log('Input : reverse(' + i + ')');
+console.log('Output : ' + reverse(i));
